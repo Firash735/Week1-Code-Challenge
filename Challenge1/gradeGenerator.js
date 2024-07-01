2bc600ed888ed3bb5b974d3ae675a2ff34da17af
Abdisharif <sharifabdi735@gmail.com>
2024-07-01 15:00:04 +0300

    created gradeGenerator using readline

--- Challenge1/gradeGenerator.js
+++ Challenge1/gradeGenerator.js
@@ -1,18 +1,39 @@
-// Function to generate student grade based on input marks 
-function generateGrade(marks) {
-    let grade;
+const readline = require("readline");
 
-    if (marks > 79) {
-        grade = 'A';
-    } else if (marks >= 60) {
-        grade = 'B';
-    } else if (marks >= 50) {
-        grade = 'C';
-    } else if (marks >= 40) {
-        grade = 'D';
-    } else {
-        grade = 'E';
-    }
+// Create an interface to read input from the user
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-    return grade;
+function generateGrade() {
+  rl.question("Enter student marks (0-100): ", function (marks) {
+    // Convert the input to a number
+    marks = Number(marks);
+
+// Variable to hold the grade
+let grade;
+
+// Determine the grade based on marks
+if (marks > 79) {
+  grade = "A";
+} else if (marks >= 60) {
+  grade = "B";
+} else if (marks >= 50) {
+  grade = "C";
+} else if (marks >= 40) {
+  grade = "D";
+} else {
+  grade = "E";
 }
+
+// Output the grade
+console.log(`The grade is: ${grade}`);
+
+// Close the readline interface
+rl.close();
+  });
+}
+
+// Call the function
+generateGrade();
